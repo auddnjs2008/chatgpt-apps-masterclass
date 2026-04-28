798786788a1084d82cb22340b5c546eb3e40d81e
auddnjs2008 <auddnjs2008@naver.com>
2026-04-29 07:24:43 +0900

    clientId

--- server/src/lib/authorize.ts
+++ server/src/lib/authorize.ts
@@ -4,6 +4,6 @@ export default async function handleAuthorizeGet(request: Request, env: Env) {
 	if (!clientId) {
 		return new Response(null, { status: 400 });
 	}
-	env.OAUTH_PROVIDER.lookupClient('');
-	return new Response();
+	const client = await env.OAUTH_PROVIDER.lookupClient(clientId);
+	return new Response(JSON.stringify(client));
 }
